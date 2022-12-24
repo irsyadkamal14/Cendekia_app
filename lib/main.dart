8741a33b0f34d57c844a38cd4f97a7771edf08fb
irsyadkamal <kamalfanan1404@gmail.com>
2022-12-24 15:09:28 +0700

    oriented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:project_premmob/screens/mainmenu/home.dart';
 import 'package:project_premmob/restApi/Loading.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -8,6 +9,9 @@ import 'package:project_premmob/screens/splashScreen/splashscreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
